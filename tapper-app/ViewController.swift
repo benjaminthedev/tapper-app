69ec6aa8460343696d6b11ca27bf5c2fd5cbb780
ben <MacBookAir@Benjamins-MacBook-Air.local>
2016-09-01 17:52:20 +0100

    Last files edits

--- tapper-app/ViewController.swift
+++ tapper-app/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Properties fool.....
+    var maxTaps: Int = 0
+    var currentTaps = 0
+    
+    //These are the outlets
     @IBOutlet weak var logoImg: UIImageView!
     @IBOutlet weak var howManyTapsTxt: UITextField!
     @IBOutlet weak var playBtn: UIButton!
@@ -17,8 +21,59 @@ class ViewController: UIViewController {
     @IBOutlet weak var tapBtn: UIButton!
     @IBOutlet weak var tapsLbl: UILabel!
     
+    @IBAction func onCoinTapped(sender: UIButton!){
+        currentTaps+=1
+        updateTapsLbl()
+        
+        if isGameOver(){
+            restartedGame()
+        }
+    }
     
+    @IBAction func onPlayBtnPressed(sender: UIButton!){
 
-
+        if howManyTapsTxt.text != nil && howManyTapsTxt.text != ""{
+            
+            logoImg.hidden = true
+            playBtn.hidden = true
+            howManyTapsTxt.hidden = true
+            
+            tapBtn.hidden = false
+            tapsLbl.hidden = false
+            
+            maxTaps = Int(howManyTapsTxt.text!)!
+            currentTaps = 0
+            
+            updateTapsLbl()
+        }
+        
+    }
+    
+    
+    func restartedGame(){
+        maxTaps = 0
+        howManyTapsTxt.text = ""
+        
+        logoImg.hidden = false
+        playBtn.hidden = false
+        howManyTapsTxt.hidden = false
+        
+        tapBtn.hidden = true
+        tapsLbl.hidden = true
+        
+    }
+    
+    func isGameOver() -> Bool{
+        if currentTaps >= maxTaps{
+            return true
+        } else{
+            return false
+        }
+    }
+    
+    func updateTapsLbl(){
+        tapsLbl.text = "\(currentTaps) Taps"
+        
+    }
 }
 
